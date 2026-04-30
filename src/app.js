2daf2174f5d47cfaac785190988f61c108b2259b
MuzamilAli0 <muzamilali@fusionwave.co>
2026-04-30 16:51:25 +0500

    update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
 import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
 import { errorHandler } from './middlewares/error.middleware.js';
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/user', userRoutes);
 app.get('/', (req, res) => res.send('API is running (ESM Mode)'));
 
 app.use(errorHandler);
